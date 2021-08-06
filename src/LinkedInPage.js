47105f677b7a0e85ea1acea8a68873083ca24073
Aman Srivastava <aman11srivastava@gmail.com>
2021-08-06 15:04:58 +0530

    Live url updated

--- src/LinkedInPage.js
+++ src/LinkedInPage.js
@@ -23,8 +23,8 @@ class LinkedInPage extends Component {
   fetchToken(data) {
     axios
       .post(
-        // `https://www.linkedin.com/oauth/v2/accessToken?client_id=${this.client_id}&client_secret=${this.client_secret}&grant_type=authorization_code&redirect_uri=https://flamboyant-pare-c16a8b.netlify.app/linkedin&code=${data.code}`,
-        `https://www.linkedin.com/oauth/v2/accessToken?client_id=${this.client_id}&client_secret=${this.client_secret}&grant_type=authorization_code&redirect_uri=https://localhost:3001/linkedin&code=${data.code}`,
+        `https://www.linkedin.com/oauth/v2/accessToken?client_id=${this.client_id}&client_secret=${this.client_secret}&grant_type=authorization_code&redirect_uri=https://flamboyant-pare-c16a8b.netlify.app/linkedin&code=${data.code}`,
+        // `https://www.linkedin.com/oauth/v2/accessToken?client_id=${this.client_id}&client_secret=${this.client_secret}&grant_type=authorization_code&redirect_uri=https://localhost:3001/linkedin&code=${data.code}`,
         
         {
           headers: {
@@ -68,8 +68,8 @@ class LinkedInPage extends Component {
       <div>
         <LinkedIn
           clientId="78l6zz843wr43t"
-          redirectUri="https://localhost:3001/linkedin"
-          // redirectUri="https://flamboyant-pare-c16a8b.netlify.app/linkedin"
+          // redirectUri="https://localhost:3001/linkedin"
+          redirectUri="https://flamboyant-pare-c16a8b.netlify.app/linkedin"
           scope="r_emailaddress,r_liteprofile"
           onFailure={this.handleFailure}
           onSuccess={this.handleSuccess}
